2caea5d5014db9e251bd42fe2171b4db70d668cf
keanh <keanh275@gmail.com>
2020-06-09 15:17:21 +0700

    add

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
